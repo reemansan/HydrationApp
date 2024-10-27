a6be98b35edd2cb0e5f7e227f4c6a67ee8eb69bb
reemansan <reeman.dev@hotmail.com>
2024-10-27 09:32:40 +0300

    Initial Commit

---
+++ HydrationApp/HydrationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrationAppApp.swift
+//  HydrationApp
+//
+//  Created by Reeman on 27/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct HydrationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HydrationAppTests/HydrationAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HydrationAppTests.swift
+//  HydrationAppTests
+//
+//  Created by Reeman on 27/10/2024.
+//
+
+import Testing
+@testable import HydrationApp
+
+struct HydrationAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
